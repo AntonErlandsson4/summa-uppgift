9fddbdaab0f506a277eca37ab5dff5146ea21c89
AntonErlandsson4 <70326194+AntonErlandsson4@users.noreply.github.com>
2022-03-03 10:19:43 +0100

    Add files via upload

---
+++ kod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace kod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath=@"C:\Users\Admin\Desktop\Summa Program\Summa.txt";
+            StreamReader sr = new StreamReader(filepath);
+
+
+            String red;
+            int summa = 0;
+            int fem = 0;
+            while ((red = sr.ReadLine()) != null){
+                summa +- int.Parse(red);
+                
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
